9f5766c48adda5efb7c69f472fd41b352fde8a8c
Alisho Muborakshoev <muborakshoev.alisho@gmail.com>
2023-01-24 23:08:43 +0600

    Implemented home controller + view

---
+++ InternshipWebTask/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using InternshipWebTask.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InternshipWebTask.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public HomeController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var authors = _context.Authors.ToList();
+        var books = _context.Books.ToList();
+        
+        var authorsInfo = authors
+            .Select(a => new { a.Id,a.LastName, a.FirstName, a.Patronymic, Count = books.Count(b => b.AuthorId == a.Id) })
+            .ToList();
+        
+        ViewBag.authors = authorsInfo;
+        return View();
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        var author = _context.Authors.FirstOrDefault(x => x.Id == id);
+
+        _context.Authors.Remove(author);
+        await _context.SaveChangesAsync();
+        return RedirectToAction("Index", "Home");
+    }
+
+    [HttpGet]
+    public IActionResult Genres()
+    {
+        var genres = _context.Books.Select(b => b.Genre).Distinct().ToList();
+        var books = _context.Books.ToList();
+
+        var genresInfo = genres
+            .Select(x => new { Genre = x, Count = books.Count(b => b.Genre == x) })
+            .ToList();
+
+        ViewBag.genres = genresInfo;
+        return View();
+    }
+}
